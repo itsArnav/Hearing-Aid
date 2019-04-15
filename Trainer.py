1c4e873eee2d511d1748ee4c681f3210c37a4408
Arnav Gupta <45306684+itsArnav@users.noreply.github.com>
2019-04-15 21:12:38 +0530

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -1,1 +1,22 @@
+import tensorflow as tf
+import tflearn
+import numpy as np
+import cv2
+from sklearn.utils import shuffle
+#Load Images from dataset
+loadedImages = []
+for i in range(0, 1000):
+    image = cv2.imread('Dataset/SwingImages/swing_' + str(i) + '.png')
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    loadedImages.append(gray_image.reshape(89, 100, 1))
 
+for i in range(0, 1000):
+    image = cv2.imread('Dataset/PalmImages/palm_' + str(i) + '.png')
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    loadedImages.append(gray_image.reshape(89, 100, 1))
+    
+
+for i in range(0, 1000):
+    image = cv2.imread('Dataset/FistImages/fist_' + str(i) + '.png')
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    loadedImages.append(gray_image.reshape(89, 100, 1))
