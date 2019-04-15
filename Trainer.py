fd93a94041c1172d6ecf54a676881e918bbece42
Arnav Gupta <45306684+itsArnav@users.noreply.github.com>
2019-04-15 21:32:24 +0530

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -2,7 +2,11 @@ import tensorflow as tf
 import tflearn
 import numpy as np
 import cv2
+from tflearn.layers.conv import conv_2d,max_pool_2d
+from tflearn.layers.core import input_data,dropout,fully_connected
+from tflearn.layers.estimator import regression
 from sklearn.utils import shuffle
+
 #Load Images from dataset
 loadedImages = []
 for i in range(0, 1000):
